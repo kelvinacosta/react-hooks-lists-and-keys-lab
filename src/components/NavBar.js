e158695b9054023d5b15f608099e7f6ac55a5cd3
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-08-01 22:15:14 -0400

    complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkMap = links.map(link=> (
+    //Remember href with the correct attribute {`#${}`}
+    <a key={link} href={`#${link}`}>{link}</a>
+  ))
+
+  return <nav>{linkMap}</nav>;
 }
 
 export default NavBar;
